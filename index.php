09166582ed17e4829902790420565d0724420853
DamianoLandi <damianolandi01@gmail.com>
2021-09-23 16:00:41 +0200

    Data e funzione

--- index.php
+++ index.php
@@ -22,5 +22,50 @@
                 };
     ?>
     <hr>
+
+    <h2>Snack 2</h2>
+
+    <?php
+
+    $students = [
+        [
+            "Nome" => "Mario",
+            "Cognome" => "Rossi",
+            "Voti" => [
+                7, 8, 5, 6
+            ],
+        ],
+        [
+            "Nome" => "Luigi",
+            "Cognome" => "Verdi",
+            "Voti" => [
+                9, 8, 7, 8
+            ],
+        ],
+        [
+            "Nome" => "Sandro",
+            "Cognome" => "Bianchi",
+            "Voti" => [
+                3, 5, 5, 7
+            ],
+        ],
+        [
+            "Nome" => "Gabriele",
+            "Cognome" => "Azzurri",
+            "Voti" => [
+                7, 8, 5, 6
+            ],
+        ],
+    ];
+
+    function get_average($array){
+        $sum = 0;
+        foreach ($array as $item){
+            $sum += $item;
+        };
+        return ($sum / count($array));
+    };
+
+    ?>
 </body>
 </html>
